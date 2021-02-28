1633cbd71c4f05cb18b6b3d016fb24cbc37a0d71
Juan Nathaniel <nathanieljuan@gmail.com>
2021-02-28 15:01:58 +0800

    Enabled download of L8Data Biome

--- jreo/dataset.py
+++ jreo/dataset.py
@@ -1,5 +1,8 @@
+import os
 import requests
 import yaml
+import requests
+import shutil
 import random
 from abc import ABC, abstractmethod
 
@@ -28,21 +31,21 @@ class Dataset(ABC):
         """
 
         assert n > 0
+        assert os.path.exists(out_dir)
         
         with open(f"./constants/{self.dataset_id}/source.yaml") as stream:
             try:
                 source_metadata = yaml.safe_load(stream)
-                url = source_metadata['url']
-                del source_metadata['url']
+                url = source_metadata["url"]
+                del source_metadata["url"]
 
                 download_list = list(source_metadata.keys())
 
                 for scene, meta in source_metadata.items():
                     # Find based on land_category
-                    if (len(land_category) > 0 and meta['category'] not in land_category) \
-                        or (len(cloud_status) > 0 and meta['cloud_status'] not in cloud_status) \
-                        or (meta['shadows'] != shadows):
-                        print(f"Remove: {scene}")
+                    if (len(land_category) > 0 and meta["category"] not in land_category) \
+                        or (len(cloud_status) > 0 and meta["cloud_status"] not in cloud_status) \
+                        or (meta["shadows"] != shadows):
                         try:
                             download_list.remove(scene)
                         except:
@@ -59,10 +62,20 @@ class Dataset(ABC):
             random.shuffle(download_list)
             download_list = download_list[0:n]
 
-        return download_list
+        download_urls = [url + file_name + ".tar.gz" for idx, file_name in enumerate(download_list)]
+
+        # Downloading in streams and chunks
+        for idx, file_url in enumerate(download_urls):
+            print(f"Downloading file {idx + 1} out of {len(download_urls)}...")
+            local_filename = f"{out_dir}/{file_url.split('/')[-1]}"
+            with requests.get(file_url, stream=True) as r:
+                with open(local_filename, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+        
+        return download_urls
 
 
 
 class L8Biome(Dataset):
     def __init__(self, **kwargs):
-        super().__init__(dataset_id='L8Biome')
+        super().__init__(dataset_id="L8Biome")
