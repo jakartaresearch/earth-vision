4b5f7c9199da7e36e86b2259a41b422b6f1940bf
rubentea16 <rstefanus16@gmail.com>
2021-05-02 00:12:28 +0700

    feature(code): convert raw dataset to tensor dataset

--- jreo/datasets/drone_deploy.py
+++ jreo/datasets/drone_deploy.py
@@ -1,8 +1,14 @@
 """Class for Drone Deploy - Semantic Segmentation."""
 import sys
 import os
+import numpy as np
+import random
+import torch
 from .images2chips import run
-from .utils import _urlretrieve
+from .utils import _urlretrieve, to_categorical
+
+from PIL import Image
+from torch.utils.data import Dataset
 
 
 class DroneDeploy():
@@ -14,6 +20,7 @@ class DroneDeploy():
         download (bool, optional): If true, downloads the dataset from the internet and
             puts it in root directory. If dataset is already downloaded, it is not
             downloaded again.
+        tensor_data (bool, optional): If true, raw dataset will converted to tensor data.
     """
 
     resources = {
@@ -21,17 +28,19 @@ class DroneDeploy():
         'dataset-medium': 'https://dl.dropboxusercontent.com/s/r0dj9mhyv4bgbme/dataset-medium.tar.gz?dl=0'
     }
 
-    def __init__(self, root: str, dataset_type='dataset-sample', download: bool = False):
+    def __init__(self, root: str, dataset_type='dataset-sample', download: bool = False, tensor_data: bool = False):
         self.root = root
         self.dataset = dataset_type
         self.filename = f'{self.dataset}.tar.gz'
 
-        if self._check_exists():
+        if download and self._check_exists():
             print(f'zipfile "{self.filename}" already exists.')
-            return
 
-        if download:
+        if download and not self._check_exists():
             self.download()
+
+        if tensor_data:
+            self.train_dataset, self.valid_dataset = self.load_dataset()
 
     def download(self):
         """Download a dataset, extract it and create the tiles."""
@@ -66,3 +75,51 @@ class DroneDeploy():
             return True
         else:
             return False
+
+    def load_dataset(self):
+        train_files = [
+            f'{self.dataset}/image-chips/{fname}' for fname in load_lines(f'{self.dataset}/train.txt')]
+        valid_files = [
+            f'{self.dataset}/image-chips/{fname}' for fname in load_lines(f'{self.dataset}/valid.txt')]
+
+        train_dataset = DroneDataset(self.dataset, train_files)
+        valid_dataset = DroneDataset(self.dataset, valid_files)
+        return train_dataset, valid_dataset
+
+
+class DroneDataset(Dataset):
+    def __init__(self, dataset, image_files):
+        self.label_path = f'{dataset}/label-chips'
+        self.image_path = f'{dataset}/image-chips'
+        self.image_files = image_files
+        random.shuffle(self.image_files)
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        image_file = self.image_files[idx]
+        label_file = image_file.replace(self.image_path, self.label_path)
+
+        image = load_img(image_file)
+        label = mask_to_classes(load_img(label_file))
+
+        tensor_image = torch.from_numpy(np.array(image))
+        tensor_label = torch.from_numpy(np.array(label))
+        return tensor_image, tensor_label
+
+    def on_epoch_end(self):
+        random.shuffle(self.image_files)
+
+
+def load_lines(fname):
+    with open(fname, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def load_img(fname):
+    return np.array(Image.open(fname))
+
+
+def mask_to_classes(mask):
+    return to_categorical(mask[:, :, 0], 6)
