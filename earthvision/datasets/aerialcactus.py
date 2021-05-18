0c0a44ad1b10e7ff8f9e244dbf95635ecebcc124
andreas chandra <phoenix.ichigo@yahoo.com>
2021-05-18 21:00:39 +0700

    base functions

--- earthvision/datasets/aerialcactus.py
+++ earthvision/datasets/aerialcactus.py
@@ -1,2 +1,18 @@
+"""Aerial Cactus Dataset"""
+
+
 class AerialCactus():
-    raise NotImplementedError
+    """Aerial Cactus <https://www.kaggle.com/c/aerial-cactus-identification> Dataset. 
+    """
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def download(self):
+        raise NotImplementedError
