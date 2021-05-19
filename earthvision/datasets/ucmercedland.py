f7d96a3c24ebf10273649d5b63851b01f6026830
Jonathan Edwin <edwin.ryth@gmail.com>
2021-05-19 22:16:16 +0700

    Add function template

--- earthvision/datasets/ucmercedland.py
+++ earthvision/datasets/ucmercedland.py
@@ -1,2 +1,28 @@
+import sys
+import os
+import numpy as np
+import random
+import cv2
+import torch
+
+from PIL import Image
+from torch.utils.data import Dataset
+
 class UCMercedLand():
-    raise NotImplementedError
+    """UC Merced Land Use Dataset.
+    <http://weegee.vision.ucmerced.edu/datasets/landuse.html>
+    """
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def download(self):
+        """download and extract file.
+        """
+        raise NotImplementedError
