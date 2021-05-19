92f768f6552e10e40978bcd433fb08be4245f671
andreas chandra <phoenix.ichigo@yahoo.com>
2021-05-19 19:38:50 +0700

    add downloader and file checker #27

--- earthvision/datasets/__init__.py
+++ earthvision/datasets/__init__.py
@@ -1,5 +1,5 @@
 """Import specific class that will use."""
 from .drone_deploy import DroneDeploy
-from .aerial_cactus import AerialCactus
+from .aerialcactus import AerialCactus
 
 __all__ = ['DroneDeploy', 'AerialCactus']
--- earthvision/datasets/aerialcactus.py
+++ earthvision/datasets/aerialcactus.py
@@ -1,10 +1,24 @@
+"""Aerial Cactus Dataset from Kaggle."""
+import os
+import posixpath
+import shutil
+from .utils import _urlretrieve
+
+
 class AerialCactus():
     """Aerial Cactus Dataset.
+
     <https://www.kaggle.com/c/aerial-cactus-identification>
     """
 
-    def __init__(self):
-        raise NotImplementedError
+    mirrors = "https://storage.googleapis.com/ossjr"
+    resources = "cactus-aerial-photos.zip"
+
+    def __init__(self, root):
+        self.root = root
+        if not self._check_exists():
+            self.download()
+            self.extract_file()
 
     def __getitem__(self, index):
         raise NotImplementedError
@@ -12,7 +26,26 @@ class AerialCactus():
     def __len__(self):
         raise NotImplementedError
 
+    def _check_exists(self):
+        self.train_path = os.path.join(
+            self.root, "cactus-aerial-photos", "training_set", "training_set")
+        self.test_path = os.path.join(
+            self.root, "cactus-aerial-photos", "validation_set", "validation_set")
+
+        return os.path.exists(os.path.join(self.train_path, "cactus")) and \
+            os.path.exists(os.path.join(self.train_path, "no_cactus")) and \
+            os.path.exists(os.path.join(self.test_path, "cactus")) and \
+            os.path.exists(os.path.join(self.test_path, "no_cactus"))
+
     def download(self):
-        """download and extract file.
-        """
-        raise NotImplementedError
+        """Download and extract file."""
+        file_url = posixpath.join(self.mirrors, self.resources)
+        _urlretrieve(file_url, self.resources)
+
+    def extract_file(self):
+        """Extract file from compressed."""
+        path_destination = os.path.join(
+            self.root, self.resources.replace(".zip", ""))
+        os.makedirs(path_destination, exist_ok=True)
+        shutil.unpack_archive(self.resources, f"{path_destination}")
+        os.remove(self.resources)
