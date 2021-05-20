4438a23239563f38f235b9fdc04a2d9a1ec80e1f
andreas chandra <phoenix.ichigo@yahoo.com>
2021-05-20 20:53:05 +0700

    add skeleton #29

--- earthvision/datasets/resisc45.py
+++ earthvision/datasets/resisc45.py
@@ -1,2 +1,21 @@
+"""RESISC45 Dataset."""
+
+
 class RESISC45():
-    raise NotImplementedError
+    def __init__(self):
+        raise NotImplementedError
+
+    def __itemget__(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __iter__(self):
+        raise NotImplementedError
+
+    def download(self):
+        raise NotImplementedError
+
+    def _check_exists(self):
+        raise NotImplementedError
