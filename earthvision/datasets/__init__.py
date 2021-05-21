bdde73d7699a0e82ae26e35656bc68e0ddf36b3a
andreas chandra <phoenix.ichigo@yahoo.com>
2021-05-21 20:27:00 +0700

    add resisc45 to init #29

--- earthvision/datasets/__init__.py
+++ earthvision/datasets/__init__.py
@@ -1,5 +1,6 @@
 """Import specific class that will use."""
 from .drone_deploy import DroneDeploy
 from .aerial_cactus import AerialCactus
+from .resisc45 import RESISC45
 
-__all__ = ['DroneDeploy', 'AerialCactus']
+__all__ = ['DroneDeploy', 'AerialCactus', 'RESISC45']
