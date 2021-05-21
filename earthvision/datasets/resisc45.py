4a8dc44f4f12fafc20b828584a0d6f405128f40c
dwiuzila <dwiuzila@gmail.com>
2021-05-21 16:43:03 +0700

    add preliminary functions #29

--- earthvision/datasets/resisc45.py
+++ earthvision/datasets/resisc45.py
@@ -1,18 +1,62 @@
 """RESISC45 Dataset."""
+import os
+import numpy as np
+import pandas as pd
+import torch
+from ..constants.RESISC45.config import CLASS_ENC
+from .utils import _urlretrieve, _to_categorical, _load_img
+from torch.utils.data import Dataset
 
 
-class RESISC45():
-    def __init__(self):
-        raise NotImplementedError
+class RESISC45(Dataset):
+    def __init__(self, root, transform=None, target_transform=None):
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+        self.class_enc = CLASS_ENC
+        
+        # download data here
+        #
+        #
 
-    def __itemget__(self):
-        raise NotImplementedError
+        self.img_labels = self.get_path_and_label()
+
+    def __getitem__(self, idx):
+        img_path = self.img_labels.iloc[idx, 0]
+        label = self.img_labels.iloc[idx, 1]
+        image = _load_img(img_path)
+        label = _to_categorical(label, len(self.class_enc))
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        image = np.array(image)
+        image = torch.from_numpy(image)
+        sample = (image, label)
+        
+        return sample
 
     def __len__(self):
-        raise NotImplementedError
+        return len(self.img_labels)
 
     def __iter__(self):
         raise NotImplementedError
+
+    def get_path_and_label(self):
+        """Return dataframe type consist of image path and corresponding label."""
+        DATA_SIZE = 700
+        category = os.listdir('NWPU-RESISC45')
+        image_path = []
+        label = []
+        for cat in category:
+            cat_enc = self.class_enc[cat]
+            label += [cat_enc] * DATA_SIZE
+            for num in range(1, DATA_SIZE+1):
+                filename = cat + '_' + str(num).zfill(3) + '.jpg'
+                image_path += [os.path.join(self.root, 'NWPU-RESISC45', cat, filename)]
+        df = pd.DataFrame({'image': image_path, 'label': label})
+
+        return df
 
     def download(self):
         raise NotImplementedError
