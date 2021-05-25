a0079636071c5d23eaac134eadd55204be31ce2a
ryzary <ryzary25@gmail.com>
2021-05-25 13:29:26 +0900

    updated eurosat.py

--- earthvision/datasets/eurosat.py
+++ earthvision/datasets/eurosat.py
@@ -7,6 +7,7 @@ import random
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
+from .utils import _urlretrieve
 
 
 class EuroSat():
@@ -16,6 +17,10 @@ class EuroSat():
     Args:
         root (string): Root directory of dataset.
     """
+
+    labels = ["AnnualCrop", "Forest", "HerbaceousVegetation", "Highway",
+                "Industrial", "Pasture", "PermanentCrop", "Residential",
+                "River", "SeaLake"]
 
     def __init__(self, root: str):
         self.root = root
@@ -33,23 +38,41 @@ class EuroSat():
         raise NotImplementedError
 
     def _check_exists(self) -> None:
-        # If the files exist, load them (TBD)
-        if os.path.exists(self.root):
-            pass
+
+        EuroSAT_path = os.path.join(self.root, "EuroSAT")
+
+        # check if all subdirectories ("AnnualCrop", "Forest", etc) exist
+        check_subdirectory = [os.path.exists(os.path.join(EuroSAT_path,label))
+                                    for label in self.labels]
+
+        # if all subdirectories exist, load the data
+        if all(check_subdirectory):
+            print('EuroSAT data already exist.') 
+            self.load_dataset()
 
         # Else, download then load them (TBD: unzip and process the data)
         else:
-            with requests.get(self.data_url, stream=True) as r:
-                with open(self.root, 'wb') as f:
-                    shutil.copyfileobj(r.raw, f)
+            # with requests.get(self.data_url, stream=True) as r:
+            #     with open(self.root, 'wb') as f:
+            #         shutil.copyfileobj(r.raw, f)
 
+            print('Downloading EuroSAT.zip')
+            self.download()
             # Unzip and process the data here
+            print('Extracting EuroSAT.zip')
+            self.extract_file()
 
-        self.load_dataset()
 
-    def download(self):
-        """Download file."""
-        raise NotImplementedError
+    #def download(self):
+    #    """Download file."""
+    #    _urlretrieve(self.data_url,filename="EuroSAT.zip") 
 
     def load_dataset(self) -> None:
         pass
+
+
+    def extract_file(self):
+        """Extract the .zip file"""
+        target_directory = os.path.join(self.root, "EuroSAT")
+        os.makedirs(target_directory, exist_ok=True)
+        shutil.unpack_archive("EuroSAT.zip",target_directory)
