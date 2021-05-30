2de523f63bdca5365343ad85940429c9bead6ae8
Juan Nathaniel <nathanieljuan@gmail.com>
2021-05-30 22:08:24 +0800

    Fixed init, _check_exists, download, and extract file

--- earthvision/datasets/eurosat.py
+++ earthvision/datasets/eurosat.py
@@ -1,13 +1,12 @@
-"""Eurosat Dataset."""
 import os
-import requests
 import shutil
+import posixpath
 import numpy as np
-import random
+import pandas as pd
 import torch
 from torch.utils.data import Dataset
-from PIL import Image
-from .utils import _urlretrieve
+from torchvision.transforms import Resize
+from .utils import _urlretrieve, _load_img
 
 
 class EuroSat():
@@ -18,61 +17,75 @@ class EuroSat():
         root (string): Root directory of dataset.
     """
 
+    mirrors = "http://madm.dfki.de/files/sentinel"
+    resources = "EuroSAT.zip"
+
     labels = ["AnnualCrop", "Forest", "HerbaceousVegetation", "Highway",
                 "Industrial", "Pasture", "PermanentCrop", "Residential",
                 "River", "SeaLake"]
 
-    def __init__(self, root: str):
+    def __init__(self,
+                root: str,
+                data_mode: str = '2750',
+                transform=Resize((64, 64)),
+                target_transform=None):
+                
         self.root = root
-        self.data_url = "http://madm.dfki.de/files/sentinel/EuroSAT.zip"
-        self._check_exists()
+        self.data_mode = data_mode
+        self.transform = transform
+        self.target_transform = target_transform
 
-    def __getitem__(self, index):
-        raise NotImplementedError
-
-    def __len__(self):
-        raise NotImplementedError
-
-    def __iter__(self):
-        """Iterator of the class."""
-        raise NotImplementedError
-
-    def _check_exists(self) -> None:
-
-        EuroSAT_path = os.path.join(self.root, "EuroSAT")
-
-        # check if all subdirectories ("AnnualCrop", "Forest", etc) exist
-        check_subdirectory = [os.path.exists(os.path.join(EuroSAT_path,label))
-                                    for label in self.labels]
-
-        # if all subdirectories exist, load the data
-        if all(check_subdirectory):
-            print('EuroSAT data already exist.') 
-            self.load_dataset()
-
-        # Else, download then load them (TBD: unzip and process the data)
-        else:
-            # with requests.get(self.data_url, stream=True) as r:
-            #     with open(self.root, 'wb') as f:
-            #         shutil.copyfileobj(r.raw, f)
-
-            print('Downloading EuroSAT.zip')
+        if not self._check_exists():
             self.download()
-            # Unzip and process the data here
-            print('Extracting EuroSAT.zip')
             self.extract_file()
 
+        self.img_labels = self.get_path_and_label()
 
-    #def download(self):
-    #    """Download file."""
-    #    _urlretrieve(self.data_url,filename="EuroSAT.zip") 
 
-    def load_dataset(self) -> None:
-        pass
+    def __getitem__(self, idx):
+        img_path = self.img_labels.iloc[idx, 0]
+        label = self.img_labels.iloc[idx, 1]
+        image = _load_img(img_path)
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        image = np.array(image)
+        image = torch.from_numpy(image)
+        sample = (image, label)
+
+        return sample
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __iter__(self):
+        for index in range(self.__len__()):
+            yield self.__getitem__(index)
+
+    def _check_exists(self) -> None:
+        self.data_path = os.path.join(
+            self.root, self.data_mode)
+
+        return os.path.exists(os.path.join(self.data_path, "AnnualCrop")) and \
+            os.path.exists(os.path.join(self.data_path, "Forest")) and \
+            os.path.exists(os.path.join(self.data_path, "HerbaceousVegetation")) and \
+            os.path.exists(os.path.join(self.data_path, "Highway")) and \
+            os.path.exists(os.path.join(self.data_path, "Industrial")) and \
+            os.path.exists(os.path.join(self.data_path, "Pasture")) and \
+            os.path.exists(os.path.join(self.data_path, "PermanentCrop")) and \
+            os.path.exists(os.path.join(self.data_path, "Residential")) and \
+            os.path.exists(os.path.join(self.data_path, "River")) and \
+            os.path.exists(os.path.join(self.data_path, "SeaLake"))
+
+
+    def download(self):
+       """Download file"""
+       file_url = posixpath.join(self.mirrors, self.resources)
+       _urlretrieve(file_url, os.path.join(self.root, self.resources))
 
 
     def extract_file(self):
         """Extract the .zip file"""
-        target_directory = os.path.join(self.root, "EuroSAT")
-        os.makedirs(target_directory, exist_ok=True)
-        shutil.unpack_archive("EuroSAT.zip",target_directory)
+        shutil.unpack_archive(os.path.join(self.root, self.resources), self.root)
+        os.remove(os.path.join(self.root, self.resources))
