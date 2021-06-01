c2796f78192dfb1110c349e4c8e45e8c0587d7ac
Jonathan Edwin <edwin.ryth@gmail.com>
2021-06-01 19:33:24 +0700

    Create template for LandCover Dataset

--- earthvision/datasets/landcover.py
+++ earthvision/datasets/landcover.py
@@ -1,2 +1,26 @@
 class LandCover():
-    raise NotImplementedError
+
+    """
+    The LandCover.ai (Land Cover from Aerial Imagery) dataset.
+    <https://landcover.ai/download/landcover.ai.v1.zip>
+    """
+
+    mirrors = "https://landcover.ai/download/"
+    resources = "landcover.ai.v1.zip"
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def download(self):
+        """download and extract file.
+        """
+        raise NotImplementedError
+
+
+  
