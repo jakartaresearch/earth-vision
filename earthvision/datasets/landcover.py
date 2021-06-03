04a49ed1c1eb121e5c07b4a647ee0ecb17336b29
Jonathan Edwin <edwin.ryth@gmail.com>
2021-06-03 21:40:32 +0700

    Add getitem

--- earthvision/datasets/landcover.py
+++ earthvision/datasets/landcover.py
@@ -1,3 +1,13 @@
+import os
+import shutil
+import posixpath
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from torchvision.transforms import Resize
+from .utils import _urlretrieve, _load_img
+
 class LandCover():
 
     """
@@ -12,10 +22,24 @@ class LandCover():
         raise NotImplementedError
 
     def __getitem__(self, index):
-        raise NotImplementedError
+        img_path = self.img_labels.iloc[idx, 0]
+        mask_path = self.img_labels.iloc[idx, 1]
+        image = _load_img(img_path)
+        mask = _load_img(mask_path)
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            mask = self.target_transform(mask)
+        image = np.array(image)
+        image = torch.from_numpy(image)
+        mask = np.array(mask)
+        mask = torch.from_numpy(mask)
+        sample = (image, mask)
+
+        return sample
 
     def __len__(self):
-        raise NotImplementedError
+        return len(self.img_labels)
 
     def download(self):
         """download and extract file.
