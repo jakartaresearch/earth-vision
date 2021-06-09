ae0dc2e904d6ee93e17519f147a66bdc236b4448
ryzary <ryzary25@gmail.com>
2021-06-09 19:48:46 +0900

    added get_path_and_label

--- earthvision/datasets/l8sparcs.py
+++ earthvision/datasets/l8sparcs.py
@@ -3,6 +3,7 @@ import shutil
 import posixpath
 import numpy as np
 import pandas as pd
+import glob
 import torch
 from torch.utils.data import Dataset
 from torchvision.transforms import Resize
@@ -24,18 +25,16 @@ class L8SPARCS():
     
     def __init__(self,
                 root: str,
-                data_mode: str = 'sending',
-                transform=Resize((64,64)),
-                target_transform=None):
+                data_mode: str = 'sending'):
         
         self.root = root
         self.data_mode = data_mode
-        self.transform = transform
-        self.target_transform = target_transform
 
         if not self._check_exists():
             self.download()
             self.extract_file()
+
+        self.img_labels = self.get_path_and_label()
 
     def _check_exists(self) -> None:
         self.data_path = os.path.join(
@@ -51,3 +50,22 @@ class L8SPARCS():
         """Extract the .zip file"""
         shutil.unpack_archive(os.path.join(self.root, self.resources), self.root)
         os.remove(os.path.join(self.root, self.resources))
+
+
+    def get_path_and_label(self):
+        """Get the path of the images and labels (masks) in a dataframe format"""
+        image_path = []
+        label = []
+
+        for image in glob.glob(os.path.join(self.root,self.data_mode,'*_photo.png')):
+            image_path.append(image)
+        
+        for mask in glob.glob(os.path.join(self.root,self.data_mode,'*_mask.png')):
+            label.append(mask)
+
+        df = pd.DataFrame({'image': sorted(image_path), 'label': sorted(label)})
+
+        return df
+
+    
+    # NOTE: do we need "transform" and "target_transform" as in eurosat.py ?
