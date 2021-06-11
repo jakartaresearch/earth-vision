5a2bdf3b5129524bab18dfc7ad4219090307ecf6
Juan Nathaniel <nathanieljuan@gmail.com>
2021-06-11 15:54:59 +0800

    Initialize L7 Irish

--- earthvision/datasets/l7irish.py
+++ earthvision/datasets/l7irish.py
@@ -1,2 +1,26 @@
+import os
+import shutil
+import posixpath
+import numpy as np
+import pandas as pd
+import glob
+import torch
+from torch.utils.data import Dataset
+from torchvision.transforms import Resize
+from PIL import Image
+from .utils import _urlretrieve, _load_img
+
 class L7Irish():
-    raise NotImplementedError
+    """Landsat 7 Irish Cloud. 
+    <https://landsat.usgs.gov/landsat-7-cloud-cover-assessment-validation-data>
+
+    Need to crawl the individual files
+
+    Args:
+        root (string): Root directory of dataset.
+    """
+    
+    def __init__(self,
+                root: str):
+        
+        self.root = root
