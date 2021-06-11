1cd922c9e0d39e857415419bf38bd347b17c59be
Fendi Suryadinata <fendi570@gmail.com>
2021-06-11 22:17:33 +0700

    Add to_chip image landcover

--- earthvision/datasets/landcover.py
+++ earthvision/datasets/landcover.py
@@ -64,8 +64,45 @@ class LandCover(Dataset):
         """Return dataframe type consist of image path and corresponding label."""
         raise NotImplementedError
 
-    def to_chip_img_mask():
-        raise NotImplementedError
+    def to_chip_img_mask(self, base):
+        # raise NotImplementedError
+        IMGS_DIR = "./{}/images".format(base)
+        MASKS_DIR = "./{}/masks".format(base)
+        OUTPUT_DIR = "./{}/output".format(base)
+
+        TARGET_SIZE = 512
+
+        img_paths = glob.glob(os.path.join(IMGS_DIR, "*.tif"))
+        mask_paths = glob.glob(os.path.join(MASKS_DIR, "*.tif"))
+
+        img_paths.sort()
+        mask_paths.sort()
+
+        os.makedirs(OUTPUT_DIR)
+        for i, (img_path, mask_path) in enumerate(zip(img_paths, mask_paths)):
+            img_filename = os.path.splitext(os.path.basename(img_path))[0]
+            mask_filename = os.path.splitext(os.path.basename(mask_path))[0]
+            img = cv2.imread(img_path)
+            mask = cv2.imread(mask_path)
+
+            assert img_filename == mask_filename and img.shape[:2] == mask.shape[:2]
+
+            k = 0
+            for y in range(0, img.shape[0], TARGET_SIZE):
+                for x in range(0, img.shape[1], TARGET_SIZE):
+                    img_tile = img[y:y + TARGET_SIZE, x:x + TARGET_SIZE]
+                    mask_tile = mask[y:y + TARGET_SIZE, x:x + TARGET_SIZE]
+
+                    if img_tile.shape[0] == TARGET_SIZE and img_tile.shape[1] == TARGET_SIZE:
+                        out_img_path = os.path.join(OUTPUT_DIR, "{}_{}.jpg".format(img_filename, k))
+                        cv2.imwrite(out_img_path, img_tile)
+
+                        out_mask_path = os.path.join(OUTPUT_DIR, "{}_{}_m.png".format(mask_filename, k))
+                        cv2.imwrite(out_mask_path, mask_tile)
+
+                    k += 1
+
+            print("Processed {} {}/{}".format(img_filename, i + 1, len(img_paths)))
 
     def download(self):
         """download and extract file.
