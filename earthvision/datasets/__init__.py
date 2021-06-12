aa4339c121984b6e06dc3ce3a8ee0839eb2960c2
rubentea16 <rstefanus16@gmail.com>
2021-06-12 21:57:48 +0700

    chore(datasets): update __init__

--- earthvision/datasets/__init__.py
+++ earthvision/datasets/__init__.py
@@ -1,9 +1,11 @@
 """Import specific class that will use."""
 from .drone_deploy import DroneDeploy
 from .aerialcactus import AerialCactus
+from .eurosat import EuroSat
 from .resisc45 import RESISC45
 from .ucmercedland import UCMercedLand
+from .l8sparcs import L8SPARCS
 from .deepsat import DeepSat
 
-__all__ = ['DroneDeploy', 'AerialCactus',
-           'RESISC45', 'UCMercedLand', 'DeepSat']
+__all__ = ['DroneDeploy', 'AerialCactus', 'RESISC45',
+           'UCMercedLand', 'EuroSat', 'L8SPARCS', 'DeepSat']
