d7c3eaed3a2fc28354ca3a9db60068a62b92a3e6
dwiuzila <dwiuzila@gmail.com>
2021-06-17 17:07:10 +0700

    add cowc class #40

--- earthvision/datasets/__init__.py
+++ earthvision/datasets/__init__.py
@@ -6,10 +6,7 @@ from .resisc45 import RESISC45
 from .ucmercedland import UCMercedLand
 from .l8sparcs import L8SPARCS
 from .deepsat import DeepSat
+from .cowc import COWC
 
 __all__ = ['DroneDeploy', 'AerialCactus', 'RESISC45',
-<<<<<<< HEAD
            'UCMercedLand', 'EuroSat', 'L8SPARCS', 'DeepSat', 'COWC']
-=======
-           'UCMercedLand', 'EuroSat', 'L8SPARCS', 'DeepSat', 'COWC']
->>>>>>> 07c0b99c9260285bfd4c7170f8222f82a304c40b
