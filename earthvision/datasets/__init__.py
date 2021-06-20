4fe4da43d97019de17de408305c37baca556b34c
ryzary <ryzary25@gmail.com>
2021-06-20 18:56:35 +0900

    updated __init__.py

--- earthvision/datasets/__init__.py
+++ earthvision/datasets/__init__.py
@@ -5,5 +5,6 @@ from .eurosat import EuroSat
 from .resisc45 import RESISC45
 from .ucmercedland import UCMercedLand
 from .l8sparcs import L8SPARCS
+from .l7irish import L7Irish
 
-__all__ = ['DroneDeploy', 'AerialCactus', 'RESISC45', 'UCMercedLand', 'EuroSat','L8SPARCS']
+__all__ = ['DroneDeploy', 'AerialCactus', 'RESISC45', 'UCMercedLand', 'EuroSat','L8SPARCS','L7Irish']
