8edad56b9a95d13364de4c63234429c164bdceea
ryzary <ryzary25@gmail.com>
2021-06-20 18:29:42 +0900

    added get_download_url, download, extract_file, check_exists, get_path_and_label methods

--- earthvision/datasets/l7irish.py
+++ earthvision/datasets/l7irish.py
@@ -8,7 +8,11 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.transforms import Resize
 from PIL import Image
+import requests
+import requests
+from bs4 import BeautifulSoup
 from .utils import _urlretrieve, _load_img
+
 
 class L7Irish():
     """Landsat 7 Irish Cloud. 
@@ -19,8 +23,70 @@ class L7Irish():
     Args:
         root (string): Root directory of dataset.
     """
-    
+
+    mirrors = 'http://landsat.usgs.gov/cloud-validation/cca_irish_2015/'
+
     def __init__(self,
                 root: str):
         
         self.root = root
+        self.download_urls = self.get_download_url()[:3] # NOTE: for now, use the first 3 urls only
+        self.resources = [url.split('/')[-1] for url in self.download_urls]
+        self.data_modes = [filename.split('.tar.gz')[0] for filename in self.resources]
+
+        if not self._check_exists():
+            self.download()
+            self.extract_file()
+        
+        self.img_labels = self.get_path_and_label()
+
+    def get_download_url(self):
+        """Get the urls to download the files.""" 
+        page = requests.get("https://landsat.usgs.gov/landsat-7-cloud-cover-assessment-validation-data")
+        soup = BeautifulSoup(page.content, 'html.parser')
+            
+        urls = [url.get('href') for url in soup.find_all('a')]
+
+        download_urls = []
+        for url in urls:
+            try: 
+                if url.endswith('.gz'):
+                    download_urls.append(url)
+            except:
+                None
+        
+        return download_urls
+
+    def download(self):
+       """Download file"""    
+       for resource in self.resources:
+           file_url = posixpath.join(self.mirrors, resource)
+           _urlretrieve(file_url, os.path.join(self.root, resource))
+    
+    def extract_file(self):
+        """Extract the .zip file"""
+        for resource in self.resources:
+            shutil.unpack_archive(os.path.join(self.root, resource), self.root)
+            os.remove(os.path.join(self.root, resource))
+    
+    def _check_exists(self):
+        is_exists = []
+
+        for data_mode in self.data_modes:
+            data_path = os.path.join(self.root, data_mode)
+            is_exists.append(os.path.exists(data_path))
+
+        return all(is_exists) 
+
+    def get_path_and_label(self):
+        """Get the path of the images and labels (masks) in a dataframe"""
+        image_path = []
+        label = []
+
+        for data_mode in self.data_modes:
+            for image in glob.glob(os.path.join(self.root,data_mode,'L7*.TIF')):
+                image_path.append(image)
+                label.append(glob.glob(os.path.join(self.root,data_mode,'*mask*')))                
+            
+        df = pd.DataFrame({'image': image_path, 'label': label})
+        return df
