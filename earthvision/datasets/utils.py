5d2cf7e306242a9daf4e889934ce667e761eb651
rubentea16 <rstefanus16@gmail.com>
2021-07-08 22:45:37 +0700

    feat: add s3 downloader, and wrapper downloader

--- earthvision/datasets/utils.py
+++ earthvision/datasets/utils.py
@@ -1,7 +1,13 @@
+import sys
+import os
 import urllib
 import numpy as np
+import boto3
+
 from tqdm import tqdm
 from PIL import Image
+from botocore import UNSIGNED
+from botocore.client import Config
 
 
 def _urlretrieve(url: str, filename: str, chunk_size: int = 1024) -> None:
@@ -13,6 +19,38 @@ def _urlretrieve(url: str, filename: str, chunk_size: int = 1024) -> None:
                         break
                     pbar.update(chunk_size)
                     fh.write(chunk)
+
+
+def s3_downloader(s3_client, local_file_name, s3_bucket, s3_object_key):
+    meta_data = s3_client.head_object(Bucket=s3_bucket, Key=s3_object_key)
+    total_length = int(meta_data.get('ContentLength', 0))
+    downloaded = 0
+
+    def progress(chunk):
+        nonlocal downloaded
+        downloaded += chunk
+        done = int(50 * downloaded / total_length)
+        sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
+        sys.stdout.flush()
+
+    print(f'Downloading {s3_object_key}')
+    with open(local_file_name, 'wb') as f:
+        s3_client.download_fileobj(
+            s3_bucket, s3_object_key, f, Callback=progress)
+
+
+def downloader(resource, root):
+    resource_type, obj = resource.split('://')[0], resource.split('://')[1]
+    dest_pth = os.path.join(root, resource.split('/')[-1])
+
+    if resource_type == 's3':
+        s3_client = boto3.client(
+            's3', config=Config(signature_version=UNSIGNED))
+        bucket = obj.split('/')[0]
+        obj_key = '/'.join(obj.split('/')[1:])
+        s3_downloader(s3_client, dest_pth, bucket, obj_key)
+    else:
+        _urlretrieve(resource, dest_pth)
 
 
 def _load_img(fname):
