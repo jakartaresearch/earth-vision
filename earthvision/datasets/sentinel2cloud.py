73ccb5835deefb32241922d7163efb058b3a91d6
Jonathan Edwin <edwin.ryth@gmail.com>
2021-07-13 19:13:22 +0700

    Change URL to real dataset URL for #33

--- earthvision/datasets/sentinel2cloud.py
+++ earthvision/datasets/sentinel2cloud.py
@@ -20,13 +20,10 @@ class Sentinel2Cloud(Dataset):
     masks<https://zenodo.org/record/4172871/files/masks.zip?download=1>
     """
 
-    mirrors = "https://storage.googleapis.com/ossjr/sentinel2"
-    resources = "subscenes.zip"
+    mirrors = "https://zenodo.org/record/4172871/files/"
+    resources = "subscenes.zip?download=1"
+    mask_resources = "masks.zip?download=1"
 
-    mask_resources = "masks.zip"
-    shapefile_resources = "shapefiles.zip"
-    thumbnail_resources = "thumbnails.zip"
-    alt_mask_resources = "alt_masks.zip"
 
     def __init__(self,
                  root: str,
@@ -60,7 +57,7 @@ class Sentinel2Cloud(Dataset):
 
     def __iter__(self):
         for index in range(self.__len__()):
-            yield self.__itemget__(index)
+            yield self.__getitem__(index)
 
     def get_image_path_and_mask_path(self):
         """Return dataframe type consist of image path and mask path."""
@@ -85,14 +82,6 @@ class Sentinel2Cloud(Dataset):
         mask_file_url = posixpath.join(self.mirrors, self.mask_resources)
         _urlretrieve(mask_file_url, os.path.join(self.root, self.mask_resources))
         
-        shapefile_file_url = posixpath.join(self.mirrors, self.shapefile_resources)
-        _urlretrieve(shapefile_file_url, os.path.join(self.root, self.shapefile_resources))
-
-        thumbnail_file_url = posixpath.join(self.mirrors, self.thumbnail_resources)
-        _urlretrieve(thumbnail_file_url, os.path.join(self.root, self.thumbnail_resources))
-        
-        altmasks_file_url = posixpath.join(self.mirrors, self.alt_mask_resources)
-        _urlretrieve(altmasks_file_url, os.path.join(self.root, self.alt_mask_resources))
 
 
     def _check_exists(self):
@@ -101,10 +90,7 @@ class Sentinel2Cloud(Dataset):
         self.data_path = os.path.join(self.root, "sentinel2cloud")
 
         return os.path.exists(os.path.join(self.data_path, "subscenes")) and \
-            os.path.exists(os.path.join(self.data_path, "masks")) and \
-            os.path.exists(os.path.join(self.data_path, "shapefiles")) and \
-            os.path.exists(os.path.join(self.data_path, "thumbnails")) and \
-            os.path.exists(os.path.join(self.data_path, "alt_masks"))
+            os.path.exists(os.path.join(self.data_path, "masks"))
 
     def extract_file(self):
         """Extract file from compressed.
@@ -120,14 +106,3 @@ class Sentinel2Cloud(Dataset):
             self.root, self.mask_resources), os.path.join(self.root, "sentinel2cloud"))
         os.remove(os.path.join(self.root, self.mask_resources))
 
-        shutil.unpack_archive(os.path.join(
-            self.root, self.shapefile_resources), os.path.join(self.root, "sentinel2cloud"))
-        os.remove(os.path.join(self.root, self.shapefile_resources))
-
-        shutil.unpack_archive(os.path.join(
-            self.root, self.thumbnail_resources), os.path.join(self.root, "sentinel2cloud"))
-        os.remove(os.path.join(self.root, self.thumbnail_resources))
-
-        shutil.unpack_archive(os.path.join(
-            self.root, self.alt_mask_resources), os.path.join(self.root, "sentinel2cloud"))
-        os.remove(os.path.join(self.root, self.alt_mask_resources))
