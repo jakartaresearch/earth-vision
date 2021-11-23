f0cff815260f0d99cc71f8a10723cca96c8a6357
rubentea16 <rstefanus16@gmail.com>
2021-11-23 19:37:46 +0700

    chore: remove test notebook

--- test.py
+++
@@ -1,7 +0,0 @@
-from earthvision import dataset
-
-try:
-    l8biome = dataset.L8Biome()
-    print("Your development environment has been setup properly")
-except:
-    print("Installation failed, please open an issue to let us know!")
