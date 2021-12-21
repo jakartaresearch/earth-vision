e2d13a7efeade7138ac0022364141177713f6a68
adhiiisetiawan <adhisetiawan518@gmail.com>
2021-12-21 10:57:12 +0700

    adding vgg16 class

--- earthvision/models/vgg16.py
+++ earthvision/models/vgg16.py
@@ -1,4 +1,8 @@
+from torch import nn
+from typing import Any
 from torchvision.models import VGG
+from torchvision.models.vgg import make_layers, cfgs
+from .utils import load_state_dict_from_url
 
 __all__ = ['VGG', 'vgg16']
 
@@ -8,3 +12,22 @@ model_urls = {
         "resisc45_vgg16.pth",
     )
 }
+
+class VGG16(VGG):
+    def __init__(
+        self,
+        features: nn.Module,
+        num_classes: int = 45,
+        init_weights: bool = True
+    ) -> None:
+        super().__init__(features, num_classes, init_weights)
+
+def _vgg(arch: str, cfg: str, batch_norm: bool, pretrained: bool, progress: bool, **kwargs: Any) -> VGG:
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm), **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
