8133c98201bf111819ac5888d5aa98cf5eb027ca
adhiiisetiawan <adhisetiawan518@gmail.com>
2021-12-21 12:09:46 +0700

    fix some error and incompatible variable

--- earthvision/models/vgg16.py
+++ earthvision/models/vgg16.py
@@ -1,8 +1,11 @@
+import torch
 from torch import nn
 from typing import Any
 from torchvision.models import VGG
-from .utils import load_state_dict_from_url
 from torchvision.models.vgg import make_layers, cfgs
+
+from .utils import load_state_dict_from_url
+
 
 __all__ = ['VGG', 'vgg16']
 
@@ -13,26 +16,29 @@ model_urls = {
     )
 }
 
-class VGG16(VGG):
+
+class VGG16Resisc45(VGG):
     def __init__(
         self,
         features: nn.Module,
         num_classes: int = 45,
         init_weights: bool = True
-    ) -> None:
-        super().__init__(features, num_classes, init_weights)
+    ):
+        super().__init__(features, num_classes=num_classes, init_weights=init_weights)
 
-def _vgg(arch: str, cfg: str, batch_norm: bool, pretrained: bool, progress: bool, **kwargs: Any) -> VGG:
+
+def _vgg(arch: str, cfg: str, batch_norm: bool, pretrained: bool, progress: bool = True, **kwargs: Any) -> VGG16Resisc45:
     if pretrained:
         kwargs['init_weights'] = False
-    model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm), **kwargs)
+    model = VGG16Resisc45(make_layers(cfgs[cfg], batch_norm=batch_norm), **kwargs)
     if pretrained:
-        state_dict = load_state_dict_from_url(model_urls[arch],
-                                              progress=progress)
-        model.load_state_dict(state_dict)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        state_dict = load_state_dict_from_url(model_urls[arch], map_location=device)
+        model.load_state_dict(state_dict, strict=False)
     return model
 
-def vgg16(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> VGG16:
+
+def vgg16(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> VGG:
     r"""VGG 16-layer model (configuration "D")
     `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_.
     The required minimum input size of the model is 32x32.
